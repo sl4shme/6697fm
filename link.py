90809644263560ae4aed2f2027763b45b11add1e
Pierre-Arthur MATHIEU <pi3rra@root.gg>
2015-03-05 20:50:50 +0000

    sounccloud

--- link.py
+++ link.py
@@ -1,20 +1,38 @@
 from datetime import datetime
+import soundcloud_mgr
+import youtube_mgr
 
 
 class Link:
-    def __init__(self, url, tag, pseudo):
-        self.url = url
-        self.tags = tag
+    def __init__(self, url, nick, playlist=None):
+        self.url = self.sanitize(url)
+        self.playlist = self.sanitize(playlist)
         self.date = datetime.now()
-        self.pseudo = pseudo
-        self.sourceType = self.getSourceType(self.url)
-        self.name = self.getInfos(self.url, self.sourceType)
+        self.nick = nick
+        self.media = self.getMedia()
+        self.infos = self.add()
 
-    def getSourceType(self, url):
-        pass
+    def sanitize(self, toClean):
+        return toClean
 
-    def getInfos(self, url, sourceType):
-        pass
+    def getMedia(self):
+        self.media = "soundcloud"
+
+    def add(self):
+        if self.media == "soundcloud":
+            self.manager = soundcloud_mgr.Sc()
+        if self.media == "youtube":
+            self.manager = youtube_mgr.Sc()
+
+        return self.manager.add(self.url, [self.nick, self.playlist])
 
     def toDict(self):
-        pass
+        data = {
+                "url": self.url,
+                "playlist": self.playlist,
+                "date": self.date,
+                "nick": self.nick,
+                "media": self.media,
+                "infos": self.infos
+                }
+        return data
--- soundc.py
+++ soundc.py
@@ -2,6 +2,8 @@ import soundcloud
 import config
 
 
+# LOG LOG LOG
+
 class Sc:
     def __init__(self):
         self.client = soundcloud.Client(client_id=config.scId,
@@ -9,9 +11,44 @@ class Sc:
                                         username=config.scUsername,
                                         password=config.scPassword)
 
-    def checkLink(self, link):
-        self.client.get("/resolve", url="https://soundcloud.com/"
-                                        "gotmyboysinthewater/princes-1")
+    def add(self, link, playlists):
+        try:
+            self.datas = self.client.get("/resolve", url=link)
+        except:
+            raise ValueError("Incorrect link: {}".format(link))
+        if self.datas.kind == "track":
+            self.addTrack(playlists)
+        elif self.datas.kind == "playlist":
+            self.client.put("/me/favorites/{}".format(self.datas.id))
+        elif self.datas.kind == "user":
+            self.client.put("/me/following/{}".format(self.datas.id))
+        else:
+            raise ValueError("Incorrect link: {}".format(link))
+        return self.toDict()
 
-    def addToPlaylist(self, playlist, link):
-        pass
+    def addTrack(self, playlists):
+        self.client.put("/me/favorites/{}".format(self.datas.id))
+        for playlistName in playlists:
+            playlistInfos = {'title': playlistName, 'sharing': "public"}
+            playlist = self.client.post('/playlists', playlist=playlistInfos)
+            track = {'tracks': map(lambda id: dict(id=id), [self.datas.id])}
+            self.client.put(playlist.uri, playlist={'tracks': track})
+
+    def toDict(self):
+        dico = {}
+        return dico
+
+
+# create a client object with access token
+client = soundcloud.Client(access_token='YOUR_ACCESS_TOKEN')
+
+# create an array of track ids
+tracks = map(lambda id: dict(id=id), [21778201, 22448500, 21928809])
+
+# get playlist
+playlist = client.get('/me/playlists')[0]
+
+# add tracks to playlist
+client.put(playlist.uri, playlist={
+    'tracks': tracks
+})
