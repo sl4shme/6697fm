933d9c2b8577029bdf0f44cb104e3450cbcba19e
Pierre-Arthur MATHIEU <pi3rra@root.gg>
2015-03-09 23:28:06 +0000

    fini youtube driver

--- youtube6697.py
+++ youtube6697.py
@@ -6,9 +6,6 @@ import lxml.etree
 
 
 class Yt:
-    def __init__(self):
-        pass
-
     def connect(self):
         self.client = gdata.youtube.service.YouTubeService()
         self.client.ssl = True
@@ -37,6 +34,9 @@ class Yt:
             if channelId:
                 self.kind = "user"
                 self.resId = channelId[0]
+                channelName = [i.attrib['content'] for i in meta if
+                               i.attrib['itemprop'] == "name"]
+                self.resTitle = channelName[0]
             else:
                 div = tree.xpath("//div[@data-playlist-id]")
                 playlistId = [i.attrib['data-playlist-id'] for i in div]
@@ -45,40 +45,74 @@ class Yt:
                     self.resId = playlistId[0]
                 else:
                     raise ValueError("Invalid link.")
-            return self.finalUrl
+        return self.finalUrl
 
     def add(self, playlists):
         self.connect()
         if self.kind == "track":
             self.addTrack(playlists)
         elif self.kind == "user":
-            self.client.put("/me/following/{}".format(self.datas.id))
+            print("Playlists are not supported for now.")
         elif self.kind == "playlist":
-            print("Playlists are not supported for now.") 
+            uri = "http://gdata.youtube.com/feeds/api/playlists/"+self.resId
+            playlist = self.client.GetYouTubePlaylistVideoFeed(uri=uri)
+            self.resTitle = playlist.title.text
+            print("Playlists are not supported for now.")
         else:
             raise ValueError("Link not track, user or playlist.")
         return self.toDict()
 
-    def addTrack(self, playlists):
+    def addTrack(self, playlistNames):
         video = self.client.GetYouTubeVideoEntry(video_id=self.resId)
         resp = self.client.AddVideoEntryToFavorites(video)
-        if not isinstance(resp, gdata.youtube.YouTubeVideoEntry):
-            raise ValueError("Error liking sound.")
-        plists = yt_service.GetYouTubePlaylistFeed(username=config.ytUsername)
+        if isinstance(resp, gdata.youtube.YouTubeVideoEntry):
+            self.resTitle = resp.title.text
+        else:
+            raise ValueError("Error when liking sound.")
 
-        for playlistName in playlists:
-            playlist_feed =
-            p = yt_service.AddPlaylist(playlistName,playlistName)
-            if isinstance(p, gdata.youtube.YouTubePlaylistEntry):
-      print 'New playlist added'
+        playlists = self.getPlaylists()
+        for playlistName in playlistNames:
+            if any(i['title'] == playlistName for i in playlists):
+                uri = [i['id'] for i in playlists if i['title'] ==
+                       playlistName][0]
+                new = False
+            else:
+                resp = self.client.AddPlaylist(playlistName, playlistName)
+                if isinstance(resp, gdata.youtube.YouTubePlaylistEntry):
+                    uri = resp.FindExtensions("playlistId")[0].text
+                    new = True
+                else:
+                    raise ValueError("Failed to create playlist.")
+            uri = "http://gdata.youtube.com/feeds/api/playlists/"+uri
+
+            if not new and (self.resTitle in self.getPlaylist(uri)):
+                print("Video already present in playlist.")
+            else:
+                resp = self.client.AddPlaylistVideoEntryToPlaylist(uri,
+                                                                   self.resId)
+                if not isinstance(resp,
+                                  gdata.youtube.YouTubePlaylistVideoEntry):
+                    raise ValueError('Failed to add video to playlist.')
+
+    def getPlaylists(self):
+        playlist_feed = self.client.GetYouTubePlaylistFeed(username='default')
+        playlists = []
+        for entry in playlist_feed.entry:
+            pId = entry.FindExtensions("playlistId")[0].text
+            title = entry.title.text
+            playlists.append({"id": pId, "title": title})
+        return playlists
+
+    def getPlaylist(self, uri):
+        playlist = self.client.GetYouTubePlaylistVideoFeed(uri=uri)
+        titles = []
+        for video in playlist.entry:
+            titles.append(video.title.text)
+        return titles
 
     def toDict(self):
-        dico = {'media': "soundcloud",
-                'kind': self.datas.kind,
-                'uri': self.datas.uri}
-        if self.datas.kind == "user":
-            dico['full_name'] = self.datas.full_name
-            dico['username'] = self.datas.username
-        else:
-            dico['title'] = self.datas.title
+        dico = {'media': "youtube",
+                'kind': self.kind,
+                'id': self.resId,
+                'title': self.resTitle}
         return dico
